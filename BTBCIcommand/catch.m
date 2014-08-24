a7b535963b4fd783e5bbab4c4f87dbd386368ae8
Takumi Kodama <s1213082@gmail.com>
2014-08-24 17:49:12 +0900

    Add "catch.m"

---
+++ BTBCIcommand/catch.m
@@ -0,0 +1,34 @@
+%% Example 3: Drive Around Table (Motor Class)
+
+%% Check toolbox installation
+% verify that the RWTH - Mindstorms NXT toolbox is installed.
+if verLessThan('RWTHMindstormsNXT', '3.00');
+    error(strcat('This program requires the RWTH - Mindstorms NXT Toolbox ' ...
+        ,'version 3.00 or greater. Go to http://www.mindstorms.rwth-aachen.de ' ...
+        ,'and follow the installation instructions!'));
+end%if
+
+%% Defaults
+COM_CloseNXT all
+h = COM_OpenNXT();
+COM_SetDefaultNXT(h);
+
+%% Open Sound
+NXT_PlayTone(440, 500);
+
+%% Open Setup
+mA = NXTMotor('A')
+mA.Power = 25;
+mA.TachoLimit = 75;
+mA.SendToNXT();
+
+mB = NXTMotor('B')
+mB.Power = 50;
+mB.TachoLimit = 105;
+mB.SendToNXT();
+
+mC = NXTMotor('C')
+mC.Power = 25;
+mC.TachoLimit = 75;
+mC.SendToNXT();
+
