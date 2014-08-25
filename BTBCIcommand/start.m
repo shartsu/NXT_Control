a172aa204e8f9c2d36fc575b60fa5e2bffddb81d
Takumi Kodama <s1213082@gmail.com>
2014-08-25 12:32:21 +0900

    Modify start.m (pause(1); at each section)

--- BTBCIcommand/start.m
+++ BTBCIcommand/start.m
@@ -11,31 +11,37 @@ while char(mssg)-48 < 6
         CloseIndex;
         CloseLittle;
         disp('result is 1')
+        pause(1);
       case '2'
         CloseThumb;
         OpenIndex;
         OpenLittle;
         disp('result is 2')
+        pause(1);
       case '3'
         CloseThumb;
         CloseIndex;
         OpenLittle;
         disp('result is 3')
+        pause(1);
       case '4'
         CloseThumb;
         OpenIndex;
         CloseLittle;
         disp('result is 4')
+        pause(1);
       case '5'
         OpenThumb;
         CloseIndex;
         CloseLittle;
         disp('result is 5')
+        pause(1);
       case '6'
         OpenThumb;
         OpenIndex;
         OpenLittle;
         disp('result is 6')
+        pause(1);
         otherwise
     end
 mssg=48;
