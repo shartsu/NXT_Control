697221061afafe3a7387eaae4ad52e3f8016ece6
Takumi Kodama <s1213082@gmail.com>
2014-08-25 11:53:45 +0900

    Add 'judo.m' and 'start.m'

---
+++ BTBCIcommand/start.m
@@ -0,0 +1,19 @@
+mssg=48;
+disp(mssg)
+%while char(mssg)-48 < 6
+    mssg = judp('receive', 3333, 1);
+    char(mssg)
+    switch char(mssg)
+      case '1'
+        disp('1');
+      case '2'
+        disp('2');
+      case '3'
+        disp('3');
+      case '4'
+        disp('4');
+        otherwise
+    end
+    disp('mssg')
+    mssg=48;
+%end
